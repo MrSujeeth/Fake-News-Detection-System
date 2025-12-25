62690d996d33fc96543d2966ab598ebc8b738717
SUJEETH <127167218+MrSujeeth@users.noreply.github.com>
2025-12-26 01:49:13 +0530

    Update FNDS.py

--- FNDS.py
+++ FNDS.py
@@ -1,8 +1,3 @@
 import numpy as np
-import pandas as pd
-import matplotlib.pyplot as plt
-import seaborn as sns
-from sklearn.model_selection import train_test_split
-from sklearn.ensemble import RandomForestRegressor
-from sklearn import metrics
+
      
